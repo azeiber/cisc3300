739c48d9297ca77fa06dfd7b58057e9ada0d3f3c
Alexa <123220954+azeiber@users.noreply.github.com>
2024-09-25 16:44:59 -0400

    adding in class 4

---
+++ in-class-4/in-class-4.js
@@ -0,0 +1,15 @@
+console.log("in-class-4.js is loaded!");
+
+const array = [1,2,3,4,5];
+
+for (let i = 0; i < array.length; i++) 
+{
+    if (array[i] % 2 === 0)
+    {
+        console.log(`${i} is even`);
+    }
+    else
+    {
+        console.log(`${i} is odd`);
+    }
+}
