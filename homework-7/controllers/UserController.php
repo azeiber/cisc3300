ea08b490a2b1c8286221cf27e49e64a13b0050fc
Alexa <123220954+azeiber@users.noreply.github.com>
2024-10-30 23:38:10 -0400

    update UserController.php

--- homework-7/controllers/UserController.php
+++ homework-7/controllers/UserController.php
@@ -3,26 +3,19 @@
 namespace Controllers;
 
 use models\UserModel;
-include __DIR__ . '/../views/users.html';
 
 class UserController {
     private $userModel;
 
-    public function __construct() 
-    {
-        $this->userModel = new UserModel();
+    public function getHTMLPage() {
+        $userModel = new UserModel();
     }
 
-
     public function showHTMLPage() {
-        if(file_exists('views/users.html'))
-        {
         include 'views/users.html';
-        }
     }
 
     public function getUsersAsJSON() {
-        $userModel = new UserModel();
         header('Content-Type: application/json');
         echo json_encode($userModel->getAllUsers());
     }
